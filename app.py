9f2bad0471cd5039a551c6c1699d43e513f402d5
kelly wolosonovich <k.wolosonovich@gmail.com>
2020-06-29 21:12:58 -0700

    created home route to test html and css files

--- app.py
+++ app.py
@@ -1,1 +1,25 @@
-"""Flask app for Cupcakes"""
+from flask import Flask, request, render_template, redirect, flash, session
+from flask_debugtoolbar import DebugToolbarExtension
+# from models import db, connect_db
+from flask_bootstrap import Bootstrap
+# from seed import seed_database
+
+app = Flask(__name__)
+Bootstrap(app)
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+app.config['SECRET_KEY'] = "secret"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql:///cupcakes"
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
+debug = DebugToolbarExtension(app)
+
+# connect_db(app)
+
+# seed_database()
+
+@app.route('/')
+def cupcakes():
+    '''Render homepage.'''
+    return render_template('cupcakes.html')
