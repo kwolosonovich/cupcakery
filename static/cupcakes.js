756fea46371590d097acf43c491e5e808ad09004
kelly wolosonovich <k.wolosonovich@gmail.com>
2020-07-02 11:10:37 -0700

    updated

--- static/cupcakes.js
+++ static/cupcakes.js
@@ -7,20 +7,19 @@ function cupcakeHTML(cupcake) {
     return `
         <div class="col mb-4">
             <div class="card h-100" style="width: 18rem;">
-              <img src="${cupcake.image}" class="card-img-top" alt="cupcake photo">
-              <div class="card-body">
+                <img src="${cupcake.image}" class="card-img-top" alt="cupcake photo">
+            <div class="card-body">
                 <h5 class="card-title">${cupcake.flavor}</h5>
-              </div>
-              <div class="card-body" id="options">
-
-                   <div class="list-group" id="myList" role="tablist">
-                        <a class="list-group-item-action list-group-item" href="/api/cupcakes/${cupcake.id}" data-toggle="list">Details</a>
-                   </div>
-                  <div class="tab-content">
-<!--                  append tab-pane with details on click-->
-                  </div>                   
-                   <button class="remove card-link btn" data-id="${cupcake.id}">Remove</button>
-              </div>
+            </div>
+            <div class="card-body" id="options">
+                <div class="list-group" id="myList" role="tablist">
+                    <a class="list-group-item-action list-group-item" href="/api/cupcakes/${cupcake.id}" data-toggle="list">Details</a>
+                </div>
+                    <div class="tab-content">
+                    <!-- append tab-pane with details on click -->
+                    </div>                   
+                <button class="remove card-link btn" data-id="${cupcake.id}">Remove</button>
+            </div>
             </div>
         </div>
     `;
@@ -67,8 +66,8 @@ $('#add-cupcake').on("click", async function (e) {
 // show cupcake details
 
 $('.details').on('click', function (e) {
-    e.preventDefault()
-    console.log('details clicked')
+    e.preventDefault();
+    console.log('details clicked');
     // addDetails();
 })
 
@@ -80,44 +79,16 @@ $('.details').on('click', function (e) {
 //     );
 // };
 
-// function removeBtn() {
-//     console.log('clicked')
-// }
 
 $("#options").on("click", '.remove', function (e) {
     e.preventDefault();
-    console.log('div');
+    console.log('div-options clicked');
 });
 
 $(".remove").on('click', async function (e) {
     e.preventDefault();
-    console.log('clicked');
+    console.log('class clicked');
 });
 
-// $("#remove-btn").on("click", function (e) {
-//     e.preventDefault();
-//     console.log(e.target
-// })
-//
-// $( "button.remove" ).click(function( event ) {
-//     console.log('body');
-// });
-
-// $( "body" ).click(function( event ) {
-//     console.log('body');
-// });
-// $("#remove-btn").click(async function(e) {
-//     e.preventDefault();
-//     let cupcake = $(e.target).data('id');
-// })
-
-// $("#remove-btn").click(function (a) {
-//     console.log('okay')
-// })
-//
-// $("a").click(function(e){
-//     e.precentDefault();
-//     console.log('clicked')
-// })
 
 $(displayCupcakes);
